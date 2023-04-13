abdcd56e8aa79e51c0dcbd27ae26fcd8f20ae236
Anthony Keith <jt1keith@gmail.com>
2023-04-12 20:55:35 -0700

    lab 7 complete

--- src/auth/router/handlers.js
+++ src/auth/router/handlers.js
@@ -10,7 +10,7 @@ async function handleSignup(req, res, next) {
       user: userRecord,
       token: userRecord.token
     };
-    res.status(200).json(output);
+    res.status(201).json(output);
   } catch (e) {
     console.error(e);
     next(e);
